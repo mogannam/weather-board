8ca1257ac96b26e4308c01ed9cf8c1966e235e24
mogannam <JoeMogannam@gmail.com>
2021-10-30 23:38:00 -0700

    add config

---
+++ assets/js/config.js
@@ -0,0 +1,17 @@
+var passApiKey = "a722c7071cf0fd51c5612637a05c1077"
+var dictStr_weather = {
+    "clear sky" : "01",
+    "few clouds" : "02",
+    "scattered clouds" : "03",
+    "broken clouds" : "04",
+    "shower rain" : "09",
+    "rain" : "10",
+    "thunderstorm" : "11",
+    "snow":"13",
+    "mist":"50"
+}
+ 
+
+//http://api.openweathermap.org/geo/1.0/direct?q=tampa&appid=a722c7071cf0fd51c5612637a05c1077
+
+//var temp = `https://api.openweathermap.org/data/2.5/onecall?lat=${27.9475}&lon=${-82.4584}&exclude=minutely,hourly,alerts&appid=${a722c7071cf0fd51c5612637a05c1077}`
